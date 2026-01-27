9f2f62ab3a68acd6997cb81a8e4e73ef4b334353
Hruaia <hruaitluangasailo20@gmail.com>
2026-01-27 23:50:39 +0530

    hohohoho

--- config/settings.py
+++ config/settings.py
@@ -212,6 +212,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = "shikshacom.noreply@gmail.com"
-EMAIL_HOST_PASSWORD = "vipfptcpycorjblx"
+EMAIL_HOST_PASSWORD = "mmqz ahmo wuvv eimj"
 
 DEFAULT_FROM_EMAIL = "Shiksha <noreply@shikshacom.com>"
