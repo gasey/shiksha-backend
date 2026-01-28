440c6803fc373b6f1065bc9119d3e214cacd4b4e
Hruaia <hruaitluangasailo20@gmail.com>
2026-01-28 23:09:01 +0530

    :::::D

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,30 +4,44 @@ from .models import User
 from .models import Profile
 from .models import Role, UserRole
 
+
+@admin.register(User)
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
 
-    list_display = ("email", "username", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active")
+    list_display = (
+        "email",
+        "username",
+        "is_verified",
+        "is_staff",
+        "is_active",
+    )
+
+    list_filter = (
+        "is_verified",
+        "is_staff",
+        "is_active",
+    )
 
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Personal info", {"fields": ("username",)}),
-        ("Permissions", {"fields": ("is_staff", "is_active",
-         "is_superuser", "groups", "user_permissions")}),
+        ("Verification", {"fields": ("is_verified", "verified_at")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "username", "password1", "password2", "is_staff", "is_active"),
-        }),
-    )
-
-    search_fields = ("email", "username")
-    ordering = ("email",)
 
 
 @admin.register(Profile)
