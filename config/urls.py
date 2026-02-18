5b46ae75428d012f5725ff416dc98380b6e84572
Hruaia <hruaitluangasailo20@gmail.com>
2026-02-18 23:01:47 +0530

    tha hram rawh aw :)ssss

--- config/urls.py
+++ config/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("courses/", include("courses.urls")),
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
