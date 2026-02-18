f58af0598c2dc14a88479a77825fd652f815b112
Hruaia <hruaitluangasailo20@gmail.com>
2026-02-18 22:59:30 +0530

    tha hram rawh aw :)ss

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MyEnrolledCoursesView, CourseSubjectsView, CourseDashboardView
+from .views import MyEnrolledCoursesView, CourseSubjectsView
 # update
 from .views import (
     CreateCourseView,
