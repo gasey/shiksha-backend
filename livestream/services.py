0e91b914815fc8aeca044a7277989c0bd75705b0
Hruaia <hruaitluangasailo20@gmail.com>
2026-02-26 14:33:04 +0530

    kasdkaskdk

--- livestream/services.py
+++ livestream/services.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from livekit.api import AccessToken, VideoGrant
+from livekit.api import AccessToken, VideoGrants
 
 
 def generate_livekit_token(user, session, is_teacher=False):
@@ -8,16 +8,16 @@ def generate_livekit_token(user, session, is_teacher=False):
         settings.LIVEKIT_API_SECRET,
     )
 
-    token.identity = str(user.id)
-    token.name = user.email
+    token.with_identity(str(user.id))
+    token.with_name(user.email)
 
-    grant = VideoGrant(
-        room=session.room_name,
+    grants = VideoGrants(
         room_join=True,
+        room=session.room_name,
         can_publish=is_teacher,
         can_subscribe=True,
     )
 
-    token.add_grant(grant)
+    token.with_grants(grants)
 
     return token.to_jwt()
