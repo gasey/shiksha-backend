f1633965e04454703c6c328c38ca3c83a361b538
Hruaia <hruaitluangasailo20@gmail.com>
2026-03-01 18:00:48 +0530

    okok

--- config/settings.py
+++ config/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
