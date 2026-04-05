3f85c68be4adc7c37192471da54bc99ea147e7e1
Hruaia <hruaitluangasailo20@gmail.com>
2026-04-06 03:20:43 +0530

    serzsssssdsdsss

--- sessions_app/views.py
+++ sessions_app/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from private_sessions.services.private_token import generate_private_token
+from sessions_app.services.private_token import generate_private_token
 
 from .models import PrivateSession, SessionParticipant, SessionRescheduleHistory, ChatMessage
 from .permissions import IsTeacher, IsStudent
